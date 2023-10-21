c30dad5525c95cd5a17fbb23e66cda6f434f49e4
wenjin <378752389@qq.com>
2023-10-21 10:02:34 +0800

    SectionTitle添加右边操作插槽

--- src/components/SectionTitle.vue
+++ src/components/SectionTitle.vue
@@ -22,34 +22,49 @@ defineProps({
 
 <template>
   <div class="section-title">
-    <h2>{{ title }} </h2>
-    <SvgIcon v-if="icon" :icon="icon"/>
+    <div class="left">
+      <h2>{{ title }} </h2>
+      <SvgIcon v-if="icon" :icon="icon"/>
+    </div>
+
+    <div class="right">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <style lang="less" scoped>
 .section-title {
   display: flex;
-  align-items: center;
-  line-height: 40px;
   margin-bottom: 20px;
 
-  &::before {
-    content: "";
-    display: block;
-    width: 10px;
-    height: 40px;
-    background-color: v-bind(barColor);
-    margin-right: 15px;
+  .left {
+    display: flex;
+    align-items: center;
+    line-height: 40px;
+    flex: 1;
+
+    &::before {
+      content: "";
+      display: block;
+      width: 10px;
+      height: 40px;
+      background-color: v-bind(barColor);
+      margin-right: 15px;
+    }
+
+    h2 {
+      color: v-bind(textColor);
+      margin-right: 5px;
+    }
+
+    .el-icon {
+      font-size: 26px;
+    }
   }
 
-  h2 {
-    color: v-bind(textColor);
-    margin-right: 5px;
-  }
-
-  .el-icon {
-    font-size: 26px;
+  .right {
+    margin: 0 auto;
   }
 }
 </style>
--- src/views/content/category/index.vue
+++ src/views/content/category/index.vue
@@ -4,11 +4,13 @@ import SectionTitle from "@/components/SectionTitle.vue";
 import {reactive, ref, onMounted} from 'vue';
 import {getCategoryPage, removeCategorys} from '@/api/content/category';
 import CategoryDetail from "@/views/content/category/components/CategoryDetail.vue";
+import {ElMessage} from "element-plus";
 
-// const searchForm = ref({
-//   id: '',
-//   categoryName: '',
-// });
+const searchForm = ref({
+  categoryName: '',
+});
+
+const tableDataLoading = ref(false)
 
 
 const pageData = reactive({
@@ -22,15 +24,15 @@ const pageData = reactive({
 const tableData = ref([])
 const addFlag = ref(false)
 const categoryDetailRef = ref(null)
-// const searchFormRef = ref(null)
+const searchFormRef = ref(null)
 
-// const onSearch = () => {
-//
-// }
-// const onReset = () => {
-//   // 在模板中必须填写 prop 字段
-//   searchFormRef.value.resetFields();
-// }
+const onSearch = () => {
+
+}
+const onReset = () => {
+  // 在模板中必须填写 prop 字段
+  searchFormRef.value.resetFields();
+}
 
 
 const onAdd = () => {
@@ -48,11 +50,20 @@ const onDelete = (row) => {
 }
 
 const loadData = async (page) => {
-  const pageDataResult = await getCategoryPage(page || {});
-  tableData.value = pageDataResult.data.dataList;
-  pageData.pageNum = pageDataResult.data.pageNum;
-  pageData.pageSize = pageDataResult.data.pageSize;
-  pageData.total = pageDataResult.data.total;
+  tableDataLoading.value = true;
+  try {
+    const pageDataResult = await getCategoryPage(page || {});
+    tableData.value = pageDataResult.data.dataList;
+    pageData.pageNum = pageDataResult.data.pageNum;
+    pageData.pageSize = pageDataResult.data.pageSize;
+    pageData.total = pageDataResult.data.total;
+  } catch (e) {
+    ElMessage.error(e.message)
+  } finally {
+    tableDataLoading.value = false;
+  }
+
+
 }
 
 const pageNumChange = (pageNum) => {
@@ -69,133 +80,75 @@ onMounted(() => {
 </script>
 
 <template>
-  <!--  <el-card class="search-card">-->
-  <!--    <template #header>-->
-  <!--      <SectionTitle title="查询搜索"/>-->
-  <!--    </template>-->
+  <el-card class="search-card" shadow="never">
+    <SectionTitle title="查询搜索"/>
 
-  <!--    &lt;!&ndash;      todo 查询表单数据&ndash;&gt;-->
-  <!--    <el-form ref="searchFormRef" :inline="true" :model="searchForm">-->
-  <!--      <el-form-item label="用户名" prop="createBy">-->
-  <!--        <el-select-->
-  <!--            v-model="searchForm.roleName"-->
-  <!--            placeholder="角色名称"-->
-  <!--            clearable-->
-  <!--        >-->
-  <!--          <el-option label="Zone one" value="shanghai"/>-->
-  <!--          <el-option label="Zone two" value="beijing"/>-->
-  <!--        </el-select>-->
-  <!--      </el-form-item>-->
-  <!--      <el-form-item label="操作时间" prop="createTime">-->
-  <!--        <el-date-picker-->
-  <!--            v-model="searchForm.createTime"-->
-  <!--            type="datetime"-->
-  <!--            placeholder="角色创建时间"-->
-  <!--            clearable-->
-  <!--        />-->
-  <!--      </el-form-item>-->
-  <!--      <el-form-item style="">-->
-  <!--        <el-button type="primary" @click="onSearch">查询</el-button>-->
-  <!--        <el-button type="default" @click="onReset">重置</el-button>-->
-  <!--        <el-button type="success" v-auth="'system:admin:add'" @click="onAdd">创建</el-button>-->
-  <!--      </el-form-item>-->
-  <!--    </el-form>-->
-  <!--  </el-card>-->
+    <!--      todo 查询表单数据-->
+    <el-form ref="searchFormRef" :inline="true" :model="searchForm">
+      <el-form-item label="用户名" prop="categoryName">
+        <el-select
+            v-model="searchForm.categoryName"
+            placeholder="目录名称"
+            clearable>
+          <el-option label="Zone one" value="shanghai"/>
+          <el-option label="Zone two" value="beijing"/>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" plain @click="onSearch">查询</el-button>
+        <el-button type="default" plain @click="onReset">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
 
-  <!--  <el-divider/>-->
-
-  <el-card class="content-card">
-    <template #header>
-      <SectionTitle title="数据列表"/>
-      <div class="operate-btn-group">
-        <el-button type="success">添加</el-button>
-      </div>
-    </template>
+  <el-card class="content-card" shadow="never">
     <!--     todo 表格数据-->
     <!--      table-layout: 固定表格宽度，让表格撑满整个父元素-->
-    <div class="table-wrapper">
-      <el-table :data="tableData" max-height="700">
-        <el-table-column type="index" label="序号" width="120"/>
-        <el-table-column prop="icon" label="目录图标">
-          <template #default="scope">
-            <el-image :src="scope.row.icon"
-                      style="width: 30px;height: 30px;"
-                      :preview-teleported="true"
-                      :preview-src-list="[scope.row.icon]"/>
-          </template>
-        </el-table-column>
-        <el-table-column prop="categoryName" label="分类名称" show-overflow-tooltip/>
-        <el-table-column prop="level" label="级别"/>
-        <el-table-column prop="count" label="商品数量"/>
-        <el-table-column prop="enable" label="发布状态"/>
-        <el-table-column prop="description" label="目录描述" show-overflow-tooltip/>
-        <el-table-column prop="order" label="发布排序顺序"/>
+
+    <SectionTitle :icon="'king-menu'" title="数据列表">
+      <template #default>
+        <el-button type="success" plain @click="onAdd">添加</el-button>
+      </template>
+    </SectionTitle>
+    <el-table :load="tableDataLoading" :data="tableData" max-height="700">
+      <el-table-column type="index" label="序号" width="120"/>
+      <el-table-column prop="icon" label="目录图标">
+        <template #default="scope">
+          <el-image :src="scope.row.icon"
+                    style="width: 30px;height: 30px;"
+                    :preview-teleported="true"
+                    :preview-src-list="[scope.row.icon]"/>
+        </template>
+      </el-table-column>
+      <el-table-column prop="categoryName" label="分类名称" show-overflow-tooltip/>
+      <el-table-column prop="level" label="级别"/>
+      <el-table-column prop="count" label="商品数量"/>
+      <el-table-column prop="enable" label="发布状态"/>
+      <el-table-column prop="description" label="目录描述" show-overflow-tooltip/>
+      <el-table-column prop="order" label="发布排序顺序"/>
 
 
-        <el-table-column label="管理" align="center">
-          <template #default="scope">
-            <el-button type="warning" size="small" @click="onEdit(scope.row)">编辑</el-button>
-            <el-button type="danger" size="small" @click="onDelete(scope.row)">删除</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-    </div>
-
-    <div class="page-wrapper">
-      <!--      分页-->
-      <el-pagination background layout="prev, pager, jumper, next, total, sizes"
-                     v-model:current-page="pageData.pageNum"
-                     v-model:page-size="pageData.pageSize"
-                     :page-sizes="pageData.pageSizeList"
-                     :total="pageData.total"
-                     @current-change="pageNumChange"
-                     @size-change="pageSizeChange"/>
-    </div>
+      <el-table-column label="管理" align="center">
+        <template #default="scope">
+          <el-button type="warning" size="small" plain @click="onEdit(scope.row)">编辑</el-button>
+          <el-button type="danger" size="small" plain @click="onDelete(scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!--      分页-->
+    <el-pagination background layout="prev, pager, jumper, next, total, sizes"
+                   v-model:current-page="pageData.pageNum"
+                   v-model:page-size="pageData.pageSize"
+                   :page-sizes="pageData.pageSizeList"
+                   :total="pageData.total"
+                   @current-change="pageNumChange"
+                   @size-change="pageSizeChange"/>
 
 
   </el-card>
-  <CategoryDetail :add-flag="addFlag" ref="categoryDetailRef" />
+  <CategoryDetail :add-flag="addFlag" ref="categoryDetailRef"/>
 </template>
 
 <style lang="less" scoped>
-
-.content-card {
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-
-  &:deep(.el-card__header) {
-    display: flex;
-    justify-content: space-between;
-
-    .operate-btn-group {
-        padding-right: 30px;
-    }
-  }
-
-  &:deep(.el-card__body) {
-    position: relative;
-    flex-grow: 1;
-
-    .table-wrapper {
-      width: calc(100% - 40px);
-      position: absolute;
-    }
-
-    .page-wrapper {
-      width: calc(100% - 40px);
-      position: absolute;
-      bottom: 0;
-      padding: 10px 0;
-      background-color: white;
-      z-index: 10;
-
-      .el-pagination {
-        justify-content: right;
-      }
-    }
-  }
-}
-
 
 </style>
